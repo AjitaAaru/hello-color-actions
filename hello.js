5cb48c084d522cea29444429c86bb1606a89ef67
AjitaAaru <rashmi.malviya@yahoo.com>
2023-08-01 13:09:13 +1000

    specified location for the changes

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from the Australia Sydney on 1 Aug 2023";
 console.log(hello());
 exports.hello = hello;
